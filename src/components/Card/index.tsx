0e14b7223669587ce83d55c0529ccbcb7fddbfe6
Brian <galyenb@vcu.edu>
2024-05-30 16:18:56 -0400

    added in nextui/base card elements provided/TODO: finish styling on cards/provide project data

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,8 +1,56 @@
-const Card = () => {
-  return <></>;
+import Image from 'next/image';
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Accordion,
+  AccordionItem,
+} from '@nextui-org/react';
+
+type ProductProps = {
+  title: string;
+  description: string;
+  objective: string;
+  strategy: string;
+  results: string;
+  img: string;
 };
 
-export default Card;
+const PortfolioCard = ({
+  title,
+  description,
+  objective,
+  strategy,
+  results,
+  img,
+}: ProductProps) => {
+  return (
+    <>
+      <Card className="grid grid-cols-2 w-96">
+        <div className="">
+          <CardHeader>{title}</CardHeader>
+          {description}
+          <Accordion className="w-40 bg-blue-700 m-4 p-4 rounded-md">
+            <AccordionItem key="1" textValue="Objectives:" title="Objectives:">
+              {objective}
+            </AccordionItem>
+            <AccordionItem key="2" title="Strategy:">
+              Strategy:
+              {strategy}
+            </AccordionItem>
+            <AccordionItem key="3" title="Results:">
+              Results:
+              {results}
+            </AccordionItem>
+          </Accordion>
+        </div>
+        <Image src={img} height={200} width={200} alt={title} />
+      </Card>
+    </>
+  );
+};
+
+export default PortfolioCard;
 
 // have variable size for example of site
 
