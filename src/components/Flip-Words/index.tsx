3b36fd699a00a54ccf5d06efed9fc651e027d421
underwoodjo <132414633+underwoodjo@users.noreply.github.com>
2024-06-13 09:51:50 -0400

    added words for flip words

--- src/components/Flip-Words/index.tsx
+++ src/components/Flip-Words/index.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { FlipWords } from "../ui/flip-words";
 
 export default function FlipWordsHero() {
-  const words = ["better", "dynamic", "comprehensive", "modern"];
+  const words = ["intuitive", "dynamic", "comprehensive", "modern"];
 
   return (
     <div className="h-[40rem] flex justify-center items-center px-4">
-      <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
-        Build
+      <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400 text-center">
+        We build
         <FlipWords words={words} /> <br />
-        websites with Aceternity UI
+        websites specializing in both full stack web development and UX design.
       </div>
     </div>
   );
