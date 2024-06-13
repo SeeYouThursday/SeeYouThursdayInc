f57b340854c5f3de8f121a0a861f38e38caf48a5
underwoodjo <132414633+underwoodjo@users.noreply.github.com>
2024-06-13 12:03:48 -0400

    changed font for flip words and added empasis to the flipped word with new font

--- src/components/ui/flip-words.tsx
+++ src/components/ui/flip-words.tsx
@@ -60,7 +60,7 @@ export const FlipWords = ({
           position: "absolute",
         }}
         className={cn(
-          "z-10 inline-block relative text-left text-sky-300 px-2",
+          "z-10 inline-block relative text-left text-sky-300 px-2 vollkorn",
           className
         )}
         key={currentWord}
