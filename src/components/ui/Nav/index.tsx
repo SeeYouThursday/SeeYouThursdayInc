df1102d0a1a207d6f90a5126d7d77c878587cb82
Brian <galyenb@vcu.edu>
2024-06-14 12:09:12 -0400

    added base navbar, TODO: make responsive for all sections

--- src/components/ui/Nav/index.tsx
+++ src/components/ui/Nav/index.tsx
@@ -41,67 +41,86 @@ const Nav = () => {
   ];
 
   return (
-    <>
-      <Navbar
-        shouldHideOnScroll={true}
-        maxWidth="full"
-        className="w-screen bg-slate-900 bg-nav"
-        classNames={{ toggleIcon: ['text-blue-200'], brand: ['rounded-full'] }}
-      >
-        <NavbarContent className="sm:hidden" justify="start">
-          <NavbarMenuToggle
-            aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+    <Navbar
+      // shouldHideOnScroll={true}
+      maxWidth="full"
+      className="w-[100svw] bg-nav"
+      classNames={{
+        toggleIcon: ['text-blue-200'],
+        brand: ['rounded-full'],
+        base: ['bg-slate-900'],
+      }}
+    >
+      <NavbarContent className="sm:hidden" justify="start">
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+        />
+        <NavbarBrand>
+          <Image
+            height={50}
+            width={50}
+            quality={100}
+            src="/Purple-SeeYouThursday.png"
+            alt="SeeYouThursday"
+            className="w-16 h-16 mt-3"
           />
+          <Image
+            height={50}
+            width={50}
+            quality={100}
+            src="/SeeYouThursdayGlass.png"
+            alt="SeeYouThursday"
+            className="w-16 h-16 mt-3"
+          />
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            PlaceHolder
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link href="#" aria-current="page">
+            PlaceHolder
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            PlaceHolder
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem>
+          <Button as={Link} href="#" className="bg-violet-400">
+            Get a Quote!
+          </Button>
+        </NavbarItem>
+        <NavbarItem className="hidden sm:flex">
           <NavbarBrand>
             <Image
               height={50}
               width={50}
               quality={100}
-              src="/Purple-SeeYouThursday.png"
+              src="/clear-webdevdesign.png"
               alt="SeeYouThursday"
-              className="w-16 h-16 mt-3"
+              className="w-16 h-16 mt-3 mb-3"
             />
           </NavbarBrand>
-        </NavbarContent>
-
-        <NavbarContent className="hidden sm:flex gap-4" justify="center">
-          <NavbarItem>
-            <Link color="foreground" href="#">
-              Features
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarMenu>
+        {navItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link className="w-full" href="#" size="lg">
+              {item.name}
             </Link>
-          </NavbarItem>
-          <NavbarItem isActive>
-            <Link href="#" aria-current="page">
-              Customers
-            </Link>
-          </NavbarItem>
-          <NavbarItem>
-            <Link color="foreground" href="#">
-              Integrations
-            </Link>
-          </NavbarItem>
-        </NavbarContent>
-        <NavbarContent justify="end">
-          <NavbarItem className="hidden lg:flex">
-            <Link href="#">Login</Link>
-          </NavbarItem>
-          <NavbarItem>
-            <Button as={Link} color="primary" href="#" variant="flat">
-              Sign Up
-            </Button>
-          </NavbarItem>
-        </NavbarContent>
-        <NavbarMenu>
-          {navItems.map((item, index) => (
-            <NavbarMenuItem key={`${item}-${index}`}>
-              <Link className="w-full" href="#" size="lg">
-                {item.name}
-              </Link>
-            </NavbarMenuItem>
-          ))}
-        </NavbarMenu>
-      </Navbar>
-    </>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
   );
 };
 
--- src/util/cn.ts
+++ src/util/cn.ts
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-import { ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-=======
 import { ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
->>>>>>> c04cceef0703c7e6d90906ca23ef4d53603045f7
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
