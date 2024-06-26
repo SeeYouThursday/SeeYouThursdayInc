31a65d1a24c35c33876ffb1174110db81edcee6b
Devon2731 <devon.whitaker27@gmail.com>
2024-06-26 15:45:49 -0400

    added inifintecard file made a new testimonal with infinite scrolls with some gradient. added some margin to the testimony

--- src/components/ui/Mission/index.tsx
+++ src/components/ui/Mission/index.tsx
@@ -6,16 +6,15 @@ import ScrollAnimation from 'react-animate-on-scroll';
 const Mission = () => {
   return (
     <div className="text-white overflow-y-auto max-h-screen flex flex-col mt-10 text-balance text-center bg-slate-900">
-      <ScrollAnimation animateIn="fadeIn" duration={1} animateOnce={true}>
+      {/* <ScrollAnimation animateIn="fadeIn" delay={0.1 * 2000} animateOnce> */}
         <h1 className="text-6xl text-center mb-7">Our Mission</h1>
-      </ScrollAnimation>
-      <ScrollAnimation
+      {/* </ScrollAnimation> */}
+      {/* <ScrollAnimation
         animateIn="fadeIn"
-        duration={1}
-        delay={500}
-        animateOnce={true}
-      >
-        <p>
+        delay={0.1 * 1000}
+        animateOnce
+      > */}
+        <p className="-translate-x-half transform px-4 pb-6 pt-6 text-center text-base transition-transform duration-500 md:px-6 md:text-lg lg:text-xl xl:text-2xl">
           At our company, we&apos;re dedicated to crafting cutting-edge
           solutions that not only empower our users but also transform lives.
           Our unwavering mission is to harness the power of technology to create
@@ -23,7 +22,7 @@ const Mission = () => {
           we&apos;re constantly innovating and pushing boundaries, all while
           ensuring timely delivery of our groundbreaking solutions.
         </p>
-      </ScrollAnimation>
+      {/* </ScrollAnimation> */}
     </div>
   );
 };
