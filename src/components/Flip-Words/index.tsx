5fe947b585a71402dc76f6f53f3a4ac50ea80da3
underwoodjo <132414633+underwoodjo@users.noreply.github.com>
2024-06-28 14:56:11 -0400

    edited opacity to flip words div

--- src/components/Flip-Words/index.tsx
+++ src/components/Flip-Words/index.tsx
@@ -9,7 +9,7 @@ export default function FlipWordsHero() {
   return (
     <div className="h-[30rem] flex justify-center items-center px-4 bg-image w-screen">
       <Parallax speed={-10}>
-        <div className="text-2xl md:text-4xl p-4 mx-auto font-normal text-violet-100 text-center w-screen text-balance poppins-regular h-full bg-slate-700">
+        <div className="text-2xl md:text-4xl p-4 mx-auto font-normal text-violet-100 text-center w-screen text-balance poppins-regular h-full bg-slate-700/70">
           Building
           <FlipWords words={words} /> <br />
           websites specializing in full-stack web development and UX design.
