1a9c02a90a9bd326e4577125cad46076815ebe0b
Brian <galyenb@vcu.edu>
2024-07-19 13:40:58 -0400

    added pill to the links on lg screens/ refactored lg links to mapping for efficieny

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import Image from "next/image";
-import Title from "@/components/Title";
-import Hero from "@/components/ui/Hero";
-import Testimonials from "@/components/ui/Testimonials";
-import Mission from "@/components/ui/Mission";
-import Pricing from "@/components/ui/Pricing";
-import { ContactModal } from "@/components/ContactForm";
-import FlipCard from "@/components/ui/MeetTheTeam";
-import Service from "@/components/ui/ServicesCard";
+import Image from 'next/image';
+import Title from '@/components/Title';
+import Hero from '@/components/ui/Hero';
+import Testimonials from '@/components/ui/Testimonials';
+import Mission from '@/components/ui/Mission';
+import Pricing from '@/components/ui/Pricing';
+import { ContactModal } from '@/components/ContactForm';
+import FlipCard from '@/components/ui/MeetTheTeam';
+import Service from '@/components/ui/ServicesCard';
 
 export default function Home() {
   return (
