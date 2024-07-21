4fd182276d1bbdf4bc1a25810fa2a6fa01f5421f
Devon2731 <devon.whitaker27@gmail.com>
2024-07-21 14:01:43 -0400

    added brian image to the public and imported it into the MeettheTeam file

--- src/components/ui/MeetTheTeam/index.tsx
+++ src/components/ui/MeetTheTeam/index.tsx
@@ -18,7 +18,7 @@ type Team = {
 };
 
 const team: Team[] = [
-  { description: 'Lead Developer', image: '/SeeYouThursdayGlass.png', rotate: 'y', subtitle: 'Founder', title: 'Brian Galyen' },
+  { description: 'Lead Developer', image: '/BrianG.webp', rotate: 'y', subtitle: 'Founder', title: 'Brian Galyen' },
   { description: 'Developer', image: '/devon-thinks.webp', rotate: 'y', subtitle: 'Co-Founder', title: 'Devon Whitaker' },
   { description: 'Designer', image: '/JojoU.webp', rotate: 'y', subtitle: 'Co-Founder', title: 'Joanna Underwood' }
 ];
