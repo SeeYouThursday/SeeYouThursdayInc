313c4d352fc86cb7872eb035255a7abcee6d01c5
Brian <galyenb@vcu.edu>
2024-07-25 15:42:15 -0400

    added location prop to ContactModal in page.tsx which causes a deploy failure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import Image from "next/image";
-import Title from "@/components/Title";
-import Hero from "@/components/ui/Hero";
-import Testimonials from "@/components/ui/Testimonials";
-import Mission from "@/components/ui/Mission";
-import { ContactModal } from "@/components/ContactForm";
-import FlipCard from "@/components/ui/MeetTheTeam";
-import Service from "@/components/ui/ServicesCard";
-import { Suspense } from "react";
+import Image from 'next/image';
+import Title from '@/components/Title';
+import Hero from '@/components/ui/Hero';
+import Testimonials from '@/components/ui/Testimonials';
+import Mission from '@/components/ui/Mission';
+import { ContactModal } from '@/components/ContactForm';
+import FlipCard from '@/components/ui/MeetTheTeam';
+import Service from '@/components/ui/ServicesCard';
+import { Suspense } from 'react';
 
 export default function Home() {
   return (
@@ -14,19 +14,19 @@ export default function Home() {
       <Title />
       {/* <Hero /> */}
       <Suspense fallback={<div>Loading...</div>}>
-      <Mission />
+        <Mission />
       </Suspense>
       <Suspense fallback={<div>Loading...</div>}>
-      <Service />
+        <Service />
       </Suspense>
       <Suspense fallback={<div>Loading...</div>}>
-      <FlipCard />
+        <FlipCard />
       </Suspense>
       <Suspense fallback={<div>Loading...</div>}>
         <Testimonials />
       </Suspense>
       <Suspense fallback={<div>Loading...</div>}>
-        <ContactModal />
+        <ContactModal location="front" />
       </Suspense>
     </main>
   );
