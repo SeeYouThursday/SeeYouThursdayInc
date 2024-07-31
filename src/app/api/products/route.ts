0994e8695d4f503d828f4119b3fc4964493c9b86
Devon2731 <devon.whitaker27@gmail.com>
2024-07-31 14:37:06 -0400

    modified the product get all route

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -3,17 +3,17 @@ import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
-export const productsRoutes = {
-    getAllProducts: async (req: NextRequest) => {
-        try {
-            const products = await prisma.product.findMany();
-           NextResponse.json(products);
-        } catch (error) {
-            NextResponse.json({ error } , {status: 500});
-        }
-    },
-
-};
     export async function GET (req: NextRequest) {
-        return productsRoutes.getAllProducts(req);
-    }
+        try {
+            await prisma.$connect();
+            const products = await prisma.product.findMany();
+           return NextResponse.json(products);
+        } catch (error) {
+           return NextResponse.json({ error } , {status: 500});
+        } finally {
+            await prisma.$disconnect();
+        }
+        
+    } 
+
+
