996eb020649b0c5f73ae03f6b3f1de0277ad7971
Brian <galyenb@vcu.edu>
2024-08-02 15:36:32 -0400

    changed id name in client.ts for inngest

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -1,4 +1,4 @@
 import { Inngest } from 'inngest';
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: 'SeeYouThursday' });
+export const inngest = new Inngest({ id: 'see-you-thursday-inc' });
