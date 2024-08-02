30e2b6fab9139ba747de5c439433d569d2cdd1a3
Brian <galyenb@vcu.edu>
2024-08-02 15:04:11 -0400

    added inngest to handle webhooks

---
+++ src/inngest/sync-user.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from '@prisma/client';
+import { inngest } from '@/inngest/client';
+
+export const syncUser = inngest.createFunction(
+  { id: 'sync-user-from-clerk' }, // ←The 'id' is an arbitrary string used to identify the function in the dashboard
+  { event: 'clerk/user.created' }, // ← This is the function's triggering event
+  async ({ event }: { event: any }) => {
+    const user = event.data; // The event payload's data will be the Clerk User json object
+    const { id } = user;
+    const email: string = user.email_addresses.find(
+      (e: { id: string }) => e.id === user.primary_email_address_id
+    ).email;
+
+    const prisma = new PrismaClient();
+    const clerk_id = id;
+
+    try {
+      const result = await prisma.admin.create({
+        data: {
+          clerk_id,
+          email,
+        },
+      });
+      return result;
+    } catch (error: any) {
+      console.log(error);
+    }
+  }
+);
