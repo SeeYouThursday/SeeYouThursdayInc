8906570e0f08a9a965241048ac629ef076462d17
Brian <galyenb@vcu.edu>
2024-08-02 15:26:33 -0400

    changed the export of api/inngest to export POST

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -3,9 +3,22 @@ import { inngest } from '@/inngest/client';
 import { syncUser } from '@/inngest/sync-user';
 
 // Create an API that serves zero functions
-export default serve({
+const { POST } = serve({
   client: inngest,
   functions: [
     syncUser, // <-- This is where you'll always add all your functions
   ],
 });
+
+export default POST;
+
+// const inngest = new Inngest({ name: 'My App' });
+// const fn = inngest.createFunction(
+//   { name: 'Welcome email' },
+//   { event: 'user.signup' },
+//   async ({ event }) => {
+//     return await sendEmail(event.data.email, 'welcome');
+//   }
+// );
+
+// export default serve(inngest, [fn]);
