073c72c98a00fac2af053b990573170842296d5b
Brian <galyenb@vcu.edu>
2024-08-02 11:22:35 -0400

    added dynamic key to clerkMiddleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+export default clerkMiddleware({secretKey: process.env.NEXT_CLERK_SECRET_KEY,});
 
 export const config = {
   matcher: [
