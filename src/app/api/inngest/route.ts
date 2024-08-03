eac666505098bddf913c628a5663060ad7379890
Brian <galyenb@vcu.edu>
2024-08-02 20:35:09 -0400

    inngest function works in dev, need to test in prod

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -3,20 +3,9 @@ import { inngest } from '@/inngest/client';
 import { syncUser } from '@/inngest/sync-user';
 
 // Create an API that serves zero functions
-export const { POST } = serve({
+export const { GET, PUT, POST } = serve({
   client: inngest,
   functions: [
     syncUser, // <-- This is where you'll always add all your functions
   ],
 });
-
-// const inngest = new Inngest({ name: 'My App' });
-// const fn = inngest.createFunction(
-//   { name: 'Welcome email' },
-//   { event: 'user.signup' },
-//   async ({ event }) => {
-//     return await sendEmail(event.data.email, 'welcome');
-//   }
-// );
-
-// export default serve(inngest, [fn]);
