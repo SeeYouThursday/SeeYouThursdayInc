bf7a62e0db7912fd9e91b09ea2e7b92b0f6ddd64
Brian <galyenb@vcu.edu>
2024-08-05 11:52:21 -0400

    added logging to errors for inngest route

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,11 +1,30 @@
 import { serve } from 'inngest/next';
 import { inngest } from '@/inngest/client';
 import { syncUser } from '@/inngest/sync-user';
+import { NextRequest, NextResponse } from 'next/server';
+let handlers;
 
-// Create an API that serves zero functions
-export const { GET, PUT, POST } = serve({
-  client: inngest,
-  functions: [
-    syncUser, // <-- This is where you'll always add all your functions
-  ],
-});
+try {
+  // Create an API that serves zero functions
+  handlers = serve({
+    client: inngest,
+    functions: [
+      syncUser, // <-- This is where you'll always add all your functions
+    ],
+  });
+} catch (error: any) {
+  console.error('Error initializing API handlers:', error);
+  handlers = {
+    GET: async (req: NextRequest, res: NextResponse) => {
+      return NextResponse.json({ error: error.message });
+    },
+    PUT: async (req: NextRequest, res: NextResponse) => {
+      return NextResponse.json({ error: error.message });
+    },
+    POST: async (req: NextRequest, res: NextResponse) => {
+      return NextResponse.json({ error: error.message });
+    },
+  };
+}
+
+export const { GET, PUT, POST } = handlers;
