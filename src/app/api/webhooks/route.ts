3cc5ed44520b7e41c515786eeb19fbcc6539a4d1
Brian <galyenb@vcu.edu>
2024-08-06 11:01:21 -0400

    fixed email address pathing on sync-user.ts causing a bug in prod

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -4,41 +4,31 @@ import { WebhookEvent } from '@clerk/nextjs/server';
 import { NextRequest, NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 
-export async function POST(req: NextRequest, res: NextResponse) {
-  // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
+export async function POST(req: NextRequest) {
   const prisma = new PrismaClient();
-
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  const WEBHOOK_SECRET = process.env.NEXT_WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
-    throw new Error(
-      'Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local'
-    );
+    console.error('WEBHOOK_SECRET is not set');
+    return new Response('Internal Server Error', { status: 500 });
   }
 
-  // Get the headers
   const headerPayload = headers();
   const svix_id = headerPayload.get('svix-id');
   const svix_timestamp = headerPayload.get('svix-timestamp');
   const svix_signature = headerPayload.get('svix-signature');
 
-  // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response('Error occured -- no svix headers', {
-      status: 400,
-    });
+    console.error('Missing svix headers');
+    return new Response('Bad Request', { status: 400 });
   }
 
-  // Get the body
   const payload = await req.json();
   const body = JSON.stringify(payload);
 
-  // Create a new Svix instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
-
   let evt: WebhookEvent;
 
-  // Verify the payload with the headers
   try {
     evt = wh.verify(body, {
       'svix-id': svix_id,
@@ -47,23 +37,18 @@ export async function POST(req: NextRequest, res: NextResponse) {
     }) as WebhookEvent;
   } catch (err) {
     console.error('Error verifying webhook:', err);
-    return new Response('Error occured', {
-      status: 400,
-    });
+    return new Response('Bad Request', { status: 400 });
   }
 
-  // Type guard to ensure evt.data has the expected properties
   if ('id' in evt.data && 'email_addresses' in evt.data) {
     const { id, email_addresses } = evt.data as {
       id: string;
       email_addresses: { email_address: string }[];
     };
-    const clerk_id: string = id!;
+    const clerk_id: string = id;
     const email = email_addresses[0].email_address;
-    console.log(id);
 
     try {
-      //TODO Write Prisma create new Admin
       const result = await prisma.admin.create({
         data: {
           clerk_id,
@@ -72,7 +57,13 @@ export async function POST(req: NextRequest, res: NextResponse) {
       });
       return NextResponse.json({ result }, { status: 200 });
     } catch (error: any) {
+      console.error('Error creating admin:', error);
       return NextResponse.json({ error: error.message }, { status: 500 });
+    } finally {
+      await prisma.$disconnect();
     }
+  } else {
+    console.error('Invalid event data');
+    return new Response('Bad Request', { status: 400 });
   }
 }
