6a6aaeb8460e953469750c75084dc377820843db
Brian <galyenb@vcu.edu>
2024-08-06 13:18:47 -0400

    updated next.config to allow imgs to be used from Vercel Blob

--- next.config.js
+++ next.config.js
@@ -1,11 +1,21 @@
+const { hostname } = require('os');
+
 module.exports = {
-    async rewrites() {
-      return [
-        {
-          source: '/api/:path*',
-          destination: '/api/admin',
-        },
-      ];
-    },
-  };
-  
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/api/admin',
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'see-you-thursday-inc-blob.public.blob.vercel-storage.com',
+        port: '',
+      },
+    ],
+  },
+};
