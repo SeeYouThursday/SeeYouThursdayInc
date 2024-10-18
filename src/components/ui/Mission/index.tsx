52d785f9f7fecebf4bea7a8c9d038b454fbbd749
Devon2731 <devon.whitaker27@gmail.com>
2024-10-18 13:41:13 -0400

    removeimage

--- src/components/ui/Mission/index.tsx
+++ src/components/ui/Mission/index.tsx
@@ -9,14 +9,14 @@ const Mission = () => {
       id="mission"
     >
       {/* <ScrollAnimation animateIn="fadeIn" delay={0.1 * 2000} animateOnce> */}
-      <h2 className="text-5xl font-bold poppins-medium text-transparent bg-gradient-to-b from-sky-600/80 via-sky-300 to-sky-400/60 text-center text-edge-outline whitespace-nowrap bg-clip-text mt-7 mb-7">Our Mission</h2>
+      <h1 className="z-10 text-5xl font-bold poppins-medium text-transparent bg-gradient-to-b from-sky-600/80 via-sky-300 to-sky-400/60 text-center text-edge-outline whitespace-nowrap bg-clip-text mt-7 mb-">Our Mission</h1>
       {/* </ScrollAnimation> */}
       {/* <ScrollAnimation
         animateIn="fadeIn"
         delay={0.1 * 1000}
         animateOnce
       > */}
-      <p className="-translate-x-half transform px-4 pb-6 pt-6 text-center text-base transition-transform duration-500 md:px-6 md:text-lg lg:text-xl xl:text-2xl">
+      <p className="-translate-x-half transform px-4 pb-16 pt-6 text-center text-base transition-transform duration-500 md:px-6 md:text-lg lg:text-xl xl:text-2xl">
         At our company, we&apos;re dedicated to crafting cutting-edge solutions
         that not only empower our users but also transform lives. Our unwavering
         mission is to harness the power of technology to create positive change
